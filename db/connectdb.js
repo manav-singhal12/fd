5f091aafa42fd6c0b1a4dc1da8fa7254381f6828
manav-singhal12 <manavsinghal165@gmail.com>
2024-06-07 12:25:09 +0530

    fd

--- db/connectdb.js
+++ db/connectdb.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 const connectDB=async()=>{
     try{
         const conn=await mongoose.connect(`mongodb://localhost:27017/sharara`,{
-            useNewUrlParser:true,
+            // useNewUrlParser:true,
+            useUnifiedTopology: true,
 
         });
         console.log(`Mongodb connexted:{conn.connection.host}`);
